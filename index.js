bcfd0b32161270b6a0ffb5f434ec786ead44c663
Jack <52157258+SaltySchnauzer@users.noreply.github.com>
2024-04-01 16:23:34 +0800

    Fixed links
    Added in rootpath because html-relative can't do inside scripts (my boy onclick needs some attention)
    Also added in partials because i got a bit distracted

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ import relative from "metalsmith-html-relative";
 import permalinks from "@metalsmith/permalinks";
 import serve from "@fidian/metalsmith-serve";
 import when from "metalsmith-if";
+import rootPath from "metalsmith-rootpath";
+import partials from "metalsmith-discover-partials";
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const isProduction = process.env.NODE_ENV == 'production';
@@ -25,15 +28,21 @@ Metalsmith(__dirname)
     .use(markdown())
 
     // Convert rest of the website into website
-    .directory(".") // Move back to root
-    .use(ancestry()) // something something builds an ancestry
+    .directory(".")     // Move back to root
+
+    .use(ancestry())    // something something builds an ancestry
     
-    // Turns absolute html links from /src as root to relative links
-    // Works in templates
-    .use(permalinks())  // this builds it all weird and then doesnt resolve the safe links correctly like i cannot get a link working to another page
-                        // i reckon i'm just stupid but its been doing my head in for like two hours
+    .use(permalinks())  // i'm not really sure what this does but it removes the trailing slash in the uri so good enough
+
+    // Change links into relative links
+    .use(relative())    // works for most links
+    .use(rootPath())    // covers edgecases for relative links (inside scripts/onclicks)
+                        // must run before layouts
+
+    // Formatting
+    .use(partials()) // adds in partials (must run before layouts)
     .use(layouts()) // use templates to build website
-    .use(relative())
+
     // Run a test server when it's not on github lol sorry github servers pre me finding out about environment variables
     .use(when(!isProduction, serve()))
 
